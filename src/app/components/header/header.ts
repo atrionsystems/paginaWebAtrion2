e6ed13ef98b05051acf274fb33fb7c540263d62a
Martin Pardo aguilar <atrionsystems@gmail.com>
2026-05-19 19:18:07 -0500

    terminada

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,5 +1,4 @@
 import { Component, HostListener } from '@angular/core';
-import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -14,8 +13,6 @@ export class HeaderComponent {
   isScrolled = false;
   scrollProgress = 0;
 
-  constructor(private router: Router) {}
-
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
     const start = 12;
@@ -24,13 +21,5 @@ export class HeaderComponent {
 
     this.scrollProgress = Math.min(Math.max(rawProgress, 0), 1);
     this.isScrolled = this.scrollProgress > 0.04;
-  }
-
-  navigateToContacto(): void {
-    this.router.navigate(['/contacto']);
-  }
-
-  navigateToServicios(): void {
-    this.router.navigate(['/servicios']);
   }
 }
